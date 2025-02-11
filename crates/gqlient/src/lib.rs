838bfb9e963c97743a2ff1b4746faf4179019a82
John T. Wodder II <git@varonathe.org>
2025-02-10 19:22:06 -0500

    gqlient: Convert to sans IO style

--- crates/gqlient/src/lib.rs
+++ crates/gqlient/src/lib.rs
@@ -1,14 +1,12 @@
 mod queries;
 mod types;
-pub use crate::queries::{Paginator, Query};
+pub use crate::queries::*;
 pub use crate::types::*;
 use anyhow::Context;
-use indenter::indented;
 use serde::{Deserialize, Serialize};
-use std::collections::VecDeque;
-use std::collections::{hash_map::Entry, HashMap};
-use std::fmt::Write;
+use std::fmt;
 use std::num::NonZeroUsize;
+use thiserror::Error;
 use ureq::{Agent, AgentBuilder};
 
 static GRAPHQL_API_URL: &str = "https://api.github.com/graphql";
@@ -59,91 +57,75 @@ impl Client {
             .map(|r| r.resources.graphql)
     }
 
-    pub fn query(&self, query: String, variables: JsonMap) -> anyhow::Result<JsonMap> {
-        let r = self
+    pub fn query(&self, payload: QueryPayload) -> Result<JsonMap, Error> {
+        let mut r = self
             .inner
             .post(GRAPHQL_API_URL)
-            .send_json(Payload { query, variables })
-            .context("failed to perform GraphQL request")?
-            .into_json::<Response>()
-            .context("failed to deserialize GraphQL response")?;
-        if !r.errors.is_empty() {
-            let mut msg = String::from("Query errored:\n");
-            let mut first = true;
-            for e in r.errors {
-                if !std::mem::take(&mut first) {
-                    writeln!(&mut msg, "---")?;
-                }
-                if let Some(t) = e.err_type {
-                    writeln!(&mut msg, "    Type: {t}")?;
-                }
-                writeln!(&mut msg, "    Message: {}", e.message)?;
-                if let Some(p) = e.path {
-                    writeln!(&mut msg, "    Path: {p:?}")?;
-                }
-            }
-            Err(anyhow::Error::msg(msg))
-        } else {
-            Ok(r.data)
-        }
+            .send_json(payload)
+            .map_err(Box::new)?
+            .into_reader();
+        let mut bytes = Vec::new();
+        r.read_to_end(&mut bytes)?;
+        serde_json::from_slice::<Response>(&bytes)?
+            .into_data()
+            .map_err(Into::into)
     }
 
-    pub fn batch_paginate<K, Q, I>(
-        &self,
-        queries: I,
-    ) -> anyhow::Result<Vec<PaginationResults<K, Q::Item>>>
-    where
-        Q: Paginator,
-        I: IntoIterator<Item = (K, Q)>,
-    {
-        let mut in_progress = queries
-            .into_iter()
-            .map(|(key, paginator)| PaginationState::new(key, paginator))
-            .collect::<VecDeque<_>>();
-        let mut results = Vec::new();
-        while !in_progress.is_empty() {
-            let mut active = HashMap::new();
-            let mut variables = JsonMap::new();
-            let mut varstr = String::new();
-            let mut qstr = String::new();
-            let mut qwrite = indented(&mut qstr).with_str("    ");
-            for (i, state) in in_progress
-                .drain(0..(in_progress.len().min(self.batch_size.get())))
-                .enumerate()
-            {
-                let alias = format!("q{i}");
-                let query = state
-                    .paginator
-                    .for_cursor(state.cursor.as_ref())
-                    .with_variable_prefix(alias.clone());
-                for (name, Variable { gql_type, value }) in query.variables() {
-                    if i > 0 {
-                        write!(&mut varstr, ", ")?;
-                    }
-                    write!(&mut varstr, "${name}: {gql_type}")?;
-                    variables.insert(name, value);
-                }
-                write!(&mut qwrite, "{alias}: ")?;
-                query.write_graphql(&mut qwrite)?;
-                active.insert(alias, ActiveQuery { state, query });
-            }
-            let full_query = format!("query ({varstr}) {{\n{qstr}}}\n");
-            let data = self.query(full_query, variables)?;
-            for (alias, value) in data {
-                let Entry::Occupied(aqo) = active.entry(alias) else {
-                    // TODO: Warn or error
-                    continue;
-                };
-                let state = aqo.remove().process_response(value)?;
-                if state.has_next_page {
-                    in_progress.push_back(state);
-                } else {
-                    results.push(PaginationResults::from(state));
+    pub fn run<Q: QueryMachine>(&self, query: Q) -> QueryResults<'_, Q> {
+        QueryResults::new(self, query)
+    }
+}
+
+#[derive(Debug)]
+pub struct QueryResults<'a, Q: QueryMachine> {
+    client: &'a Client,
+    query: Q,
+    yielding: std::vec::IntoIter<Q::Output>,
+}
+
+impl<'a, Q: QueryMachine> QueryResults<'a, Q> {
+    fn new(client: &'a Client, query: Q) -> Self {
+        QueryResults {
+            client,
+            query,
+            yielding: Vec::new().into_iter(),
+        }
+    }
+}
+
+impl<Q: QueryMachine> Iterator for QueryResults<'_, Q> {
+    type Item = Result<Q::Output, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(value) = self.yielding.next() {
+                return Some(Ok(value));
+            } else {
+                let payload = self.query.get_next_query()?;
+                match self.client.query(payload) {
+                    Ok(data) => match self.query.handle_response(data) {
+                        Ok(()) => {
+                            self.yielding = self.query.get_output().into_iter();
+                        }
+                        Err(e) => return Some(Err(e.into())),
+                    },
+                    Err(e) => return Some(Err(e)),
                 }
             }
         }
-        Ok(results)
     }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("failed to perform GraphQL request")]
+    Http(#[from] Box<ureq::Error>),
+    #[error("failed to read GraphQL response")]
+    Read(#[from] std::io::Error),
+    #[error("failed to deserialize GraphQL response")]
+    Json(#[from] serde_json::Error),
+    #[error("GraphQL server returned error response")]
+    GraphQL(#[from] GqlError),
 }
 
 // This can't be replaced with Singleton because the JSON contains more than
@@ -173,7 +155,7 @@ impl RateLimit {
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize)]
-struct Payload {
+pub struct QueryPayload {
     query: String,
     variables: JsonMap,
 }
@@ -183,79 +165,56 @@ struct Response {
     #[serde(default)]
     data: JsonMap,
     #[serde(default)]
-    errors: Vec<GraphQLError>,
+    errors: GqlError,
 }
 
+impl Response {
+    fn into_data(self) -> Result<JsonMap, GqlError> {
+        if self.errors.is_empty() {
+            Ok(self.data)
+        } else {
+            Err(self.errors)
+        }
+    }
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq)]
+#[serde(transparent)]
+pub struct GqlError(Vec<GqlInnerError>);
+
+impl GqlError {
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl fmt::Display for GqlError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Query errored:")?;
+        let mut first = true;
+        for e in &self.0 {
+            if !std::mem::take(&mut first) {
+                writeln!(f, "---")?;
+            }
+            if let Some(ref t) = e.err_type {
+                writeln!(f, "    Type: {t}")?;
+            }
+            writeln!(f, "    Message: {}", e.message)?;
+            if let Some(ref p) = e.path {
+                writeln!(f, "    Path: {p:?}")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for GqlError {}
+
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq)]
-struct GraphQLError {
+struct GqlInnerError {
     #[serde(default, rename = "type")]
     err_type: Option<String>,
     message: String,
     #[serde(default)]
     path: Option<Vec<String>>,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct PaginationState<K, P: Paginator> {
-    key: K,
-    paginator: P,
-    items: Vec<P::Item>,
-    cursor: Option<Cursor>,
-    has_next_page: bool,
-}
-
-impl<K, P: Paginator> PaginationState<K, P> {
-    fn new(key: K, paginator: P) -> Self {
-        PaginationState {
-            key,
-            paginator,
-            items: Vec::new(),
-            cursor: None,
-            has_next_page: true,
-        }
-    }
-}
-
-// Implementing these traits requires matching bounds on P::Query, which the
-// derive macros don't handle, so the traits can only be implemented manually
-// here — but I don't need them and that's busywork, so…
-//#[derive(Clone, Debug, Eq, PartialEq)]
-struct ActiveQuery<K, P: Paginator> {
-    state: PaginationState<K, P>,
-    query: P::Query,
-}
-
-impl<K, P: Paginator> ActiveQuery<K, P> {
-    fn process_response(
-        mut self,
-        value: serde_json::Value,
-    ) -> Result<PaginationState<K, P>, serde_json::Error> {
-        let page = self.query.parse_response(value)?;
-        self.state.items.extend(page.items);
-        if page.end_cursor.is_some() {
-            // endCursor is null when the page has no items, which happens when
-            // the current cursor is already at the end, so don't update the
-            // cursor to null.
-            self.state.cursor = page.end_cursor;
-        }
-        self.state.has_next_page = page.has_next_page;
-        Ok(self.state)
-    }
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct PaginationResults<K, T> {
-    pub key: K,
-    pub items: Vec<T>,
-    pub end_cursor: Option<Cursor>,
-}
-
-impl<K, Q: Paginator> From<PaginationState<K, Q>> for PaginationResults<K, Q::Item> {
-    fn from(value: PaginationState<K, Q>) -> Self {
-        PaginationResults {
-            key: value.key,
-            items: value.items,
-            end_cursor: value.cursor,
-        }
-    }
 }
--- crates/gqlient/src/queries.rs
+++ crates/gqlient/src/queries.rs
@@ -1,10 +1,18 @@
-use crate::types::{Cursor, Page, Variable};
+use crate::types::{Cursor, JsonMap, Page, Variable};
+use crate::QueryPayload;
+use indenter::indented;
+use std::collections::{
+    VecDeque,
+    {hash_map::Entry, HashMap},
+};
+use std::fmt::Write;
+use std::num::NonZeroUsize;
 
 pub trait Query: Sized {
     type Output;
 
     fn with_variable_prefix(self, prefix: String) -> Self;
-    fn write_graphql<W: std::fmt::Write>(&self, s: W) -> std::fmt::Result;
+    fn write_graphql<W: Write>(&self, s: W) -> std::fmt::Result;
     fn variables(&self) -> impl IntoIterator<Item = (String, Variable)>;
     fn parse_response(&self, value: serde_json::Value) -> Result<Self::Output, serde_json::Error>;
 }
@@ -14,4 +22,165 @@ pub trait Paginator {
     type Item;
 
     fn for_cursor(&self, cursor: Option<&Cursor>) -> Self::Query;
+}
+
+pub trait QueryMachine {
+    type Output;
+
+    fn get_next_query(&mut self) -> Option<QueryPayload>;
+    fn handle_response(&mut self, data: JsonMap) -> Result<(), serde_json::Error>;
+    fn get_output(&mut self) -> Vec<Self::Output>;
+}
+
+#[allow(missing_debug_implementations)]
+pub struct BatchPaginator<K, P: Paginator> {
+    in_progress: VecDeque<PaginationState<K, P>>,
+    results: Vec<PaginationResults<K, P::Item>>,
+    active: HashMap<String, ActiveQuery<K, P>>,
+    batch_size: NonZeroUsize,
+}
+
+impl<K, P: Paginator> BatchPaginator<K, P> {
+    pub fn new<I: IntoIterator<Item = (K, P)>>(queries: I, batch_size: NonZeroUsize) -> Self {
+        let in_progress = queries
+            .into_iter()
+            .map(|(key, paginator)| PaginationState::new(key, paginator))
+            .collect::<VecDeque<_>>();
+        let results = Vec::new();
+        let active = HashMap::new();
+        BatchPaginator {
+            in_progress,
+            results,
+            active,
+            batch_size,
+        }
+    }
+}
+
+impl<K, P: Paginator> QueryMachine for BatchPaginator<K, P> {
+    type Output = PaginationResults<K, P::Item>;
+
+    fn get_next_query(&mut self) -> Option<QueryPayload> {
+        if self.in_progress.is_empty() {
+            return None;
+        }
+        let mut variables = JsonMap::new();
+        let mut varstr = String::new();
+        let mut qstr = String::new();
+        let mut qwrite = indented(&mut qstr).with_str("    ");
+        for (i, state) in self
+            .in_progress
+            .drain(0..(self.in_progress.len().min(self.batch_size.get())))
+            .enumerate()
+        {
+            let alias = format!("q{i}");
+            let query = state
+                .paginator
+                .for_cursor(state.cursor.as_ref())
+                .with_variable_prefix(alias.clone());
+            for (name, Variable { gql_type, value }) in query.variables() {
+                if i > 0 {
+                    write!(&mut varstr, ", ").expect("writing to a string should not fail");
+                }
+                write!(&mut varstr, "${name}: {gql_type}")
+                    .expect("writing to a string should not fail");
+                variables.insert(name, value);
+            }
+            write!(&mut qwrite, "{alias}: ").expect("writing to a string should not fail");
+            query
+                .write_graphql(&mut qwrite)
+                .expect("writng to a string should not fail");
+            self.active.insert(alias, ActiveQuery { state, query });
+        }
+        let full_query = format!("query ({varstr}) {{\n{qstr}}}\n");
+        Some(QueryPayload {
+            query: full_query,
+            variables,
+        })
+    }
+
+    fn handle_response(&mut self, data: JsonMap) -> Result<(), serde_json::Error> {
+        for (alias, value) in data {
+            let Entry::Occupied(aqo) = self.active.entry(alias) else {
+                // TODO: Warn or error
+                continue;
+            };
+            let state = aqo.remove().process_response(value)?;
+            if state.has_next_page {
+                self.in_progress.push_back(state);
+            } else {
+                self.results.push(PaginationResults::from(state));
+            }
+        }
+        Ok(())
+    }
+
+    fn get_output(&mut self) -> Vec<Self::Output> {
+        self.results.drain(..).collect()
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct PaginationState<K, P: Paginator> {
+    key: K,
+    paginator: P,
+    items: Vec<P::Item>,
+    cursor: Option<Cursor>,
+    has_next_page: bool,
+}
+
+impl<K, P: Paginator> PaginationState<K, P> {
+    fn new(key: K, paginator: P) -> Self {
+        PaginationState {
+            key,
+            paginator,
+            items: Vec::new(),
+            cursor: None,
+            has_next_page: true,
+        }
+    }
+}
+
+// Implementing these traits requires matching bounds on P::Query, which the
+// derive macros don't handle, so the traits can only be implemented manually
+// here — but I don't need them and that's busywork, so…
+//#[derive(Clone, Debug, Eq, PartialEq)]
+struct ActiveQuery<K, P: Paginator> {
+    state: PaginationState<K, P>,
+    query: P::Query,
+}
+
+impl<K, P: Paginator> ActiveQuery<K, P> {
+    fn process_response(
+        mut self,
+        value: serde_json::Value,
+    ) -> Result<PaginationState<K, P>, serde_json::Error> {
+        let page = self.query.parse_response(value)?;
+        self.state.items.extend(page.items);
+        if page.end_cursor.is_some() {
+            // endCursor is null when the page has no items, which happens when
+            // the current cursor is already at the end, so don't update the
+            // cursor to null.
+            self.state.cursor = page.end_cursor;
+        }
+        self.state.has_next_page = page.has_next_page;
+        Ok(self.state)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct PaginationResults<K, T> {
+    pub key: K,
+    pub items: Vec<T>,
+    pub end_cursor: Option<Cursor>,
+}
+
+impl<K, Q: Paginator> From<PaginationState<K, Q>> for PaginationResults<K, Q::Item> {
+    fn from(value: PaginationState<K, Q>) -> Self {
+        PaginationResults {
+            key: value.key,
+            items: value.items,
+            end_cursor: value.cursor,
+        }
+    }
 }
